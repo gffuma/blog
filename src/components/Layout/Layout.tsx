52cdeaf29ccecafa0f855bc7653521b06f0ebc43
gffuma <fumagalli.gf@gmail.com>
2022-07-16 00:08:01 +0200

    Fix meta tags in home ... needs investigation

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,36 +2,26 @@ import './Layout.scss'
 import { Suspense } from 'react'
 import { Link, Outlet, useMatch } from 'react-router-dom'
 import ThemeSwitcher from '../ThemeSwitcher'
-import { Helmet } from 'react-helmet'
 
 export default function Layout() {
   const isHomePage = Boolean(useMatch('/'))
   return (
-    <>
-      <Helmet defer={false}>
-        <title>Gio Va</title>
-        <meta
-          name="description"
-          content="My name is Gio Va and i am a programmer."
-        />
-      </Helmet>
-      <Suspense fallback={<div>Loading...</div>}>
-        <div className="container">
-          <header>
-            <div className="sidebar">
-              <Link to="/" className="home-link">
-                {isHomePage ? <h1>Gio Va</h1> : <h2>Gio Va</h2>}
-              </Link>
-              <div className="sidebar-links">
-                <a href="https://github.com/gffuma">GitHub</a>
-                <a href="https://twitter.com/gffuma">Twitter</a>
-              </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="container">
+        <header>
+          <div className="sidebar">
+            <Link to="/" className="home-link">
+              {isHomePage ? <h1>Gio Va</h1> : <h2>Gio Va</h2>}
+            </Link>
+            <div className="sidebar-links">
+              <a href="https://github.com/gffuma">GitHub</a>
+              <a href="https://twitter.com/gffuma">Twitter</a>
             </div>
-            <ThemeSwitcher />
-          </header>
-          <Outlet />
-        </div>
-      </Suspense>
-    </>
+          </div>
+          <ThemeSwitcher />
+        </header>
+        <Outlet />
+      </div>
+    </Suspense>
   )
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,6 +2,7 @@ import './Home.scss'
 import Markdown from '../../components/Markdown'
 import { usePosts } from '../../hooks/posts'
 import PostListItem from '../../components/PostListItem'
+import { Helmet } from 'react-helmet'
 
 const tagline = `
 \`\`\`js
@@ -19,10 +20,17 @@ export default function App() {
   const posts = usePosts()
   return (
     <div>
+      <Helmet defer={false}>
+        <title>Gio Va</title>
+        <meta
+          name="description"
+          content="My name is Gio Va and i am a programmer."
+        />
+      </Helmet>
       <div className="hban">
         <Markdown md={tagline} />
       </div>
-      <div className='posts-list'>
+      <div className="posts-list">
         {posts.map((post) => (
           <PostListItem key={post.data.slug} post={post} />
         ))}
